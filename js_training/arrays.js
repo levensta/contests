9a4090d80e2221c01169b8acb49ba8d4da0b5517
levensta <levensta@student.21-school.ru>
2022-01-25 23:31:03 +0300

    add arrays mini-tasks

--- js_training/arrays.js
+++ js_training/arrays.js
@@ -24,10 +24,7 @@ console.log(isEvenArray(oddArr)); // false
 */
 
 function multipleOfFive(arr) {
-    return arr.map(item => {
-        if (item % 5 === 0)
-            return item;
-    });
+    return arr.filter(item => item % 5 === 0);
 }
 
 console.log(multipleOfFive([1, 2, 5, 12, 15, 21]));
@@ -37,14 +34,58 @@ console.log(multipleOfFive([1, 2, 5, 12, 15, 21]));
 * и функция возращает среднее арифметическое, (округлить результат до десятых)
 */
 
+function meanValue(arr) {
+    // let sum = 0;
+    // for (let num of arr) {
+    //     sum += num;
+    // }
+    let sum = arr.reduce((acc, curr) => acc + curr);
+    return Number((sum / arr.length).toFixed(1));
+}
+
+console.log(meanValue([1,2,3,4,5]));
+
 /*
 * Написать функцию, которая принимает массив чисел, например [1,2,3,4,5]
 * и переносит первый элемент массива в конец (например можно засунуть первый
-* элемент в конец, затем удалить первый элемент), попробовать несколькими способами
+* элемент в конец, затем удалить первый элемент)
 */
+
+function pushShit(arr) {
+    if (arr) {
+        arr.push(arr.shift());
+    }
+}
+
+let myArr = [1, 2, 3, 4, 5];
+console.log(myArr);
+pushShit(myArr);
+console.log(myArr);
 
 /*
 * Написать функцию, которая принимает массив сотрудников, каждый сотрудник имеет
-* имя и возраст ([{name: 'Иван', age: 23},...]) и возвращает массим, где каждый элемент
-* представляет из себя строку "Имя: Иван, возвраст: 23".
+* имя и возраст ([{name: 'Иван', age: 23},...]) и возвращает массив, где каждый элемент
+* представляет из себя строку "Имя: Иван, возраст: 23".
 */
+
+function parsePeople(employees) {
+    return employees.map(item => "Имя: " + item.name + ", возраст: " + item.age);
+}
+
+let arrMembers = [
+    {
+        name: "Иван",
+        age: 23
+    },
+    {
+        name: "Сергей",
+        age: 21
+    },
+    {
+        name: "Руслан",
+        age: 22
+    }
+];
+
+console.log(arrMembers);
+console.log(parsePeople(arrMembers));
